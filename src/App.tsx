834b47f1ceec980d584900507d9fe483355056c3
LAPTOP-JE44P5SO\Lenovo <-->
2023-07-09 15:39:26 +0800

    fix: merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import './App.scss';
 import { Suspense } from 'react';
-import NProgress from './components/NProgress';
 import Footer from '@/components/Footer';
 import { useRoutes } from 'react-router-dom';
 import routers from './router/routerMap';
+import Web3Provider from '@/components/WalletConnect';
+import Header from '@/components/Header';
+import { Spin } from 'antd';
 function App() {
   return (
     <div className="h-full flex flex-col-center">
-      <Suspense fallback={<NProgress />}>{useRoutes(routers)}</Suspense>
-      <Footer></Footer>
+      <Web3Provider>
+        <Header></Header>
+        <Suspense fallback={<Spin />}>{useRoutes(routers)}</Suspense>
+        <Footer></Footer>
+      </Web3Provider>
     </div>
   );
 }
